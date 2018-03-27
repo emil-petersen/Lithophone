97315fc19e3004d0f0aa67a0c6738bfe9ff6a70d
Emil Petersen <emil.petersen@thalmic.com>
2018-03-27 13:20:42 -0400

    Finalize initial implementation

--- src/lithophone.py
+++ src/lithophone.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python3.6
-import sqlite3
+# -*- coding: utf-8 -*-
 import json
+import logging
+import sqlite3
+from array import array
 
 from functools import lru_cache
+
+
+@lru_cache(maxsize=1)
+def get_logger():
+    logger = logging.getLogger(__name__)
+    return logger
 
 
 @lru_cache(maxsize=1)
 def get_db_connection(db_file):
     db = sqlite3.connect(db_file, check_same_thread=False)
     db.execute("""CREATE TABLE IF NOT EXISTS webhook_messages (
-                    target, message)""")
+                    target, message, content_type)""")
     db.commit()
     return db
 
 
 def app(env, start_response):
-
+    logger = get_logger()
 
     target = env['PATH_INFO'][1:]
     if not target:
@@ -29,34 +38,30 @@ def app(env, start_response):
     cursor = db.cursor()
 
     if env['REQUEST_METHOD'] == 'POST':
-
+        logging.info("Got post for '{}'".format(target))
+        message = env['wsgi.input'].read()
         cursor.execute(
-            'INSERT INTO webhook_messages VALUES (?, ?)', (target,
-                env['wsgi.input'].read()))
+            'INSERT INTO webhook_messages VALUES (?, ?, ?)', (target,
+                                                              message, None))
 
         db.commit()
         cursor.close()
 
         start_response('200 OK',
-            [('Content-Type', 'application/text')])
+                       [('Content-Type', 'text/plain')])
         return []
-
 
     elif env['REQUEST_METHOD'] == 'GET':
         received = cursor.execute("""
             SELECT rowid, message FROM
             webhook_messages WHERE
-            target = '{}'""".format( target)).fetchall()
-
+            target = '{}'""".format(target)).fetchall()
 
         messages = []
         message_ids = []
         for message_id, message in received:
             message_ids.append(str(message_id))
-            try:
-                messages.append(json.loads(message))
-            except ValueError:
-                messages.append(message)
+            messages.append(message.decode('utf-8'))
 
         cursor.execute(
             "DELETE FROM webhook_messages WHERE rowid IN ({})".format(
@@ -65,9 +70,20 @@ def app(env, start_response):
         db.commit()
         cursor.close()
 
+        if len(messages) == 0:
+            start_response('204 NO CONTENT', [
+                ('Content-Type', 'text/plain')])
+            return []
+
+        response = [bytes(json.dumps(messages), 'utf-8')]
+        logger.info("Target '{}' has {} messages: {}".format(target,
+                                                             len(messages), response))
+
         start_response('200 OK', [
-            ( 'Content-Type', 'application/json')])
-        return [bytes(json.dumps([str(m) for m in messages]), 'utf-8')]
+            ('Content-Type', 'application/json')])
+
+        return response
+
     else:
         start_response('400 BAD REQUEST', [])
         return []
